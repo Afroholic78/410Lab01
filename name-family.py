4bb07ee6d030752e571c54455881d9ff75c118b1
Afroholic78 <mickael.zerihoun@gmail.com>
2015-01-14 18:09:19 -0700

    added the files

---
+++ name-family.py
@@ -0,0 +1,24 @@
+class Student:
+	courseMarks={}
+	name=""
+	average = 0
+	def __init__(self,name,family):
+		self.name = name + " " +family
+
+	def addCourseMark(self, course, mark):
+		self.courseMarks[course] = mark
+
+	def average(self):
+		average = 0;
+		for x in self.courseMarks:
+			average += self.courseMarks[x]
+		self.average = average/len(self.courseMarks)
+
+S = Student("Ash", "Ketchum")
+S.addCourseMark("410", 4)
+S.addCourseMark("401", 0)
+S.average()
+print(S.name)
+print(S.courseMarks)
+print(S.average)
+
